23a9369906e75365acb0b4ab3816772164720f6a
Aigerim <t_sa06@mail.ru>
2023-02-15 19:48:52 +0600

    Добавила класс Game

--- FirstGame/Program.cs
+++ FirstGame/Program.cs
@@ -1,8 +1,43 @@
-﻿namespace FirstGame;
+﻿
+namespace FirstGame;
 class Program
 {
     static void Main()
     {
+        private static Game _game = new Game();
+
+    static void Main()
+    {
+        while (_game.GetWinner() == Winner.None)
+        {
+            PrintGame();
+            char name = char.Parse(Console.ReadLine());
+            _game.Turn(name);
+            Console.WriteLine(_game.counter);
+        }
+        PrintGame();
+
+        Winner winner = _game.GetWinner();
+        if (winner == Winner.None)
+        {
+            Console.WriteLine("Победитель ничья");
+        }
+        else if (winner == Winner.Win)
+        {
+            Console.WriteLine("Вы победили");
+
+        }
+        else
+        {
+            Console.WriteLine("Вы проиграли");
+        }
+        Console.ReadKey(true);
+    }
+
+    static void PrintGame()
+    {
+        Console.WriteLine("Угадайте слово");
+        Console.WriteLine(_game.result);
 
     }
 }
